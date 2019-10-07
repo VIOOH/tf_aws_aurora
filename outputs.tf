5c08911541493096254a0cf8b3e4b2b71a810116
torz <richard.szabo@viooh.com>
2019-10-07 14:42:55 +0100

    terraform 0.12 update

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,16 @@
 output "rds_cluster_id" {
-  value = "${aws_rds_cluster.aurora.id}"
+  value = aws_rds_cluster.aurora.id
 }
 
 output "writer_endpoint" {
-  value = "${aws_rds_cluster.aurora.endpoint}"
+  value = aws_rds_cluster.aurora.endpoint
 }
 
 output "reader_endpoint" {
-  value = "${aws_rds_cluster.aurora.reader_endpoint}"
+  value = aws_rds_cluster.aurora.reader_endpoint
 }
 
-output "security_group_id"{
-  value = "${aws_security_group.aurora_security_group.id}"
+output "security_group_id" {
+  value = aws_security_group.aurora_security_group.id
 }
+
